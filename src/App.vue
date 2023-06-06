727f36422c894e10cebf108b898bccc3b03b4ded
supersakana <zacwilliamson@icloud.com>
2023-06-06 15:55:13 -0600

    added nav component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,20 @@
 <template>
   <div class="h-screen bg-main bg-cover bg-center">
-    <nav class="flex justify-between bg-white p-5">
-      <router-link to="/">
-        <img src="./assets/logo.png" alt="logo" class="w-[40px] h-[40px]">
-      </router-link>
-
-      <div id="nav-links" class="flex items-center justify-center gap-x-3 text-sm md:text-lg duration-500">
-        <router-link to="/">Home</router-link>
-        <router-link to="/products">Products</router-link>
-        <router-link to="/services">Services</router-link>
-      </div>
-
-      <router-link to="/cart">
-        <ion-icon name="cart-outline" class="font-bold text-3xl"></ion-icon>
-      </router-link>
-    </nav>
-  <router-view/>
-
+    <Nav />
+    <router-view/>
   </div>
 </template>
+
+<script>
+import Nav from './components/Nav.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Nav
+  }
+}
+</script>
 
 <style>
 #app {
---
+++ src/components/Nav.vue
@@ -0,0 +1,24 @@
+<template>
+    <nav class="flex justify-between bg-white p-5">
+      <router-link to="/">
+        <img src="../assets/logo.png" alt="logo" class="w-[40px] h-[40px]">
+      </router-link>
+
+      <div id="nav-links" class="flex items-center justify-center gap-x-3 text-sm md:text-lg duration-500">
+        <router-link to="/">Home</router-link>
+        <router-link to="/products">Products</router-link>
+        <router-link to="/services">Services</router-link>
+      </div>
+
+      <router-link to="/cart">
+        <ion-icon name="cart-outline" class="font-bold text-3xl"></ion-icon>
+      </router-link>
+    </nav>
+</template>
+  
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Nav',
+}
+</script>
