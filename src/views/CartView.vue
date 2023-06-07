f2820ef546d5c8a108ed7552a943017db2ecc755
supersakana <zacwilliamson@icloud.com>
2023-06-07 15:33:27 -0600

    different message displays on cart view if cart is empty or not

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,17 +1,29 @@
 <template>
     <div class="cart">
-      <h1 class="text-2xl font-bold text-center mb-6">Your cart</h1>
+      <h2 v-if="cart.length < 1" class="text-2xl font-bold text-center mb-6">Your cart is empty!</h2>
+      <h2 v-else class="text-2xl font-bold text-center mb-6">Your cart</h2>
       <CartProducts />
     </div>
 </template>
 
 <script>
 import CartProducts from '@/components/CartProducts.vue';
+import { computed } from 'vue'
+import { useProductStore } from '@/store/products';
 
 export default {
   name: 'CartView',
   components: {
     CartProducts
+  },
+  setup() {
+    const storeProducts = useProductStore()
+     const cart = computed(() => storeProducts.cart)
+
+    return{
+      storeProducts,
+      cart
+    }
   },
 }
 </script>
